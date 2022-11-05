f62ffb36722f60701cee171d0f8792f19f32bfb0
John <JohnJanisheski99@gmail.com>
2022-11-05 16:31:56 -0400

    test commit 1

--- app.py
+++ app.py
@@ -1,18 +1,16 @@
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
 
 st.title("KCF Tech Dashboard")
 
 @st.cache
-def load_temps(nrows):
-    data = pd.read_csv(r"/home/sahas/Documents/Git/KCFHack/Data_set/HackPSU/Fan 1/Temperature.csv", nrows=nrows, header=None)
+def load_temps():
+    data = pd.read_csv(r"Data_set/HackPSU/Fan 1/Temp.csv", header=None)
     data.columns = ["Time", "Temp"]
-    #data.drop(data[data[' DeltaP'] == 999999].index, inplace = True)
     return data
 
 data_load_state = st.text('Loading data...')
-temp_data = load_temps(17190)
+temp_data = load_temps()
 data_load_state.text('Loading data...done!')
 
 #Line chart for Temps
