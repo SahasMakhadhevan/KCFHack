f7d0dc35882d85154ebdbbce4517867d1b10294b
MrSinister8632 <yash.saxena2001@gmail.com>
2022-11-06 01:37:02 -0500

    Update data_norm.py

--- data_norm.py
+++ data_norm.py
@@ -8,9 +8,10 @@ data = DataLoading.load_temp(path)
 
 def norm_oneweek():
     data.Time = pd.to_datetime(data.Time)
-    result = data.resample('W', on="Time").Temp.mean()
-    end = result.tail(4)
-    print(end)
+    data.Temp = pd.to_float(data.Temp)
+    result = data.resample('W-FRI', on="Time").Temp.describe()
+    end = result.head(10)
+    return end
 
 def norm_oneday():
     data['mean'] = data.groupby(pd.cut(data['Temp'], range(data.loc[0, "Time"], data.loc[0, "Time"] + constants.ONEDAY)))['Temp'].transform('mean')
