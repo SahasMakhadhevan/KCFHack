48b9505d98352dec54c43a243a4c7a4a3168361d
MrSinister8632 <yash.saxena2001@gmail.com>
2022-11-06 02:07:24 -0500

    Update data_norm.py

--- data_norm.py
+++ data_norm.py
@@ -6,19 +6,26 @@ path = r"E:\Hackathon\KCFHack\Data_set\HackPSU\Fan 1\Temperature.csv"
 
 data = DataLoading.load_temp(path)
 
+
 def norm_oneweek():
     data.Time = pd.to_datetime(data.Time)
-    data.Temp = pd.to_float(data.Temp)
+    data.Temp = pd.to_numeric(data.Temp)
     result = data.resample('W-FRI', on="Time").Temp.describe()
     end = result.head(10)
     return end
 
+
 def norm_oneday():
-    data['mean'] = data.groupby(pd.cut(data['Temp'], range(data.loc[0, "Time"], data.loc[0, "Time"] + constants.ONEDAY)))['Temp'].transform('mean')
-    return(data)
+    data['mean'] = \
+    data.groupby(pd.cut(data['Temp'], range(data.loc[0, "Time"], data.loc[0, "Time"] + constants.ONEDAY)))[
+        'Temp'].transform('mean')
+    return (data)
 
-def norm_oneweek():
-    data['mean'] = data.groupby(pd.cut(data['temp'], range(data.loc[0, "Time"], data.loc[0, "Time"] + constants.ONEWEEK)))['temp'].transform('mean')
-    return("one week")
+def norm_onehour():
+    data.Time = pd.to_datetime(data.Time)
+    data.Temp = pd.to_numeric(data.Temp)
+    result = data.resample('60T', on="Time").Temp.describe()
+    end = result.head(10)
+    return end
 
-
+print(norm_oneweek())
