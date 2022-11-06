996c5ab1a194a1ec23123fab05a32ea022779634
John <JohnJanisheski99@gmail.com>
2022-11-05 23:55:44 -0400

    Renamed Home.py to Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -1,18 +1,27 @@
 import streamlit as st
 import pandas as pd
 import DataLoading
+import altair as alt
 
 st.title("Dashboard")
 
 selected_fan = st.sidebar.selectbox("Fan Selector", ["Fan 1", "Fan 2", "Fan 3", "Fan 4", "Fan 5", "Fan 6", "Fan 7"])
-selected_time = st.sidebar.selectbox("Time Units", ["Day", "Week", "Month"])
+selected_aggregate = st.sidebar.selectbox("Aggregate", ["Day", "Week", "Month"])
+#Aggregrating on Day
+
+#switch selected_aggregate:
+
 
 
 data_load_state = st.text('Loading data...')
-temperature_data = DataLoading.load_temps(r"Data_set/HackPSU/" + selected_fan + "/Temperature.csv")
+temperature_data = DataLoading.load_temps(selected_fan)
 data_load_state.text('Loading data...done!')
 
 st.write(temperature_data.describe())
 
 # Line chart for Temps
-st.line_chart(data=temperature_data, x="Time", y="Temp", width=0, height=0, use_container_width=True)
+linechart = alt.Chart(temperature_data).mark_line().encode(y="Temp", x="Time")
+st.altair_chart(linechart, use_container_width=True)
+
+#st.write(temperature_data.plot.line())
+#st.line_chart(data=temperature_data, x="Time", y="Temp", width=0, height=0, use_container_width=True)
--- DataLoading.py
+++ DataLoading.py
@@ -7,8 +7,33 @@ def convertTime(tmpTime):
     bar = time.strftime("%a, %d %b %Y %H:%M:%S", foo)
     return bar
 
-def load_temps(path):
-    data = pd.read_csv(path, header=None)
+def load_temps(selected_fan):
+    data = pd.read_csv(r"Data_set/HackPSU/" + selected_fan + "/Temperature.csv", header=None, nrows=50)
+    data.columns = ["Time", "Temp"]
+
+    #Replaces epoch time in col 0 with formatted string dateTime
+    for index, row in data.iterrows():
+        data.loc[index, "Time"] = convertTime(int(str(row["Time"])[:-5]))
+
+    return data
+
+
+def load_X_Accelleration(selected_fan):
+    data = pd.read_csv(r"Data_set/HackPSU/" + selected_fan + "/Temperature.csv", header=None)
+    data.columns = ["Time", "Temp"]
+    return data
+
+def load_Y_Accelleration(selected_fan):
+    data = pd.read_csv(r"Data_set/HackPSU/" + selected_fan + "/Temperature.csv", header=None)
+    data.columns = ["Time", "Temp"]
+    return data
+def load_X_Velocity(selected_fan):
+    data = pd.read_csv(r"Data_set/HackPSU/" + selected_fan + "/Temperature.csv", header=None)
+    data.columns = ["Time", "Temp"]
+    return data
+
+def load_Y_Velocity(selected_fan):
+    data = pd.read_csv(r"Data_set/HackPSU/" + selected_fan + "/Temperature.csv", header=None)
     data.columns = ["Time", "Temp"]
 
     #Replaces epoch time in col 0 with formatted string dateTime
