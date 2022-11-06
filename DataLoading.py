9dbef7497daef284ad5a6a814cd2946dfb7b6088
MrSinister8632 <yash.saxena2001@gmail.com>
2022-11-06 01:48:49 -0400

    Update DataLoading.py

--- DataLoading.py
+++ DataLoading.py
@@ -1,6 +1,41 @@
 import pandas as pd
+import time
 
-def load_temps():
-    data = pd.read_csv(r"Data_set/HackPSU/Fan 1/Temperature.csv", header=None)
+
+def convertTime(tmpTime):
+    foo = time.gmtime(tmpTime)
+    bar = time.strftime("%Y %b %d %H:%M:%S", foo)
+    return bar
+
+
+def load_temp(path):
+    data = pd.read_csv(path, header=None)
     data.columns = ["Time", "Temp"]
+
+    # Replaces epoch time in col 0 with formatted string dateTime
+    for index, row in data.iterrows():
+        data.loc[index, "Time"] = convertTime(int(str(row["Time"])[:-5]))
+
+    return data
+
+
+def load_Acceleration(path):
+    data = pd.read_csv(path, header=None)
+    data.columns = ["Time", "Acceleration"]
+
+    # Replaces epoch time in col 0 with formatted string dateTime
+    for index, row in data.iterrows():
+        data.loc[index, "Time"] = convertTime(int(str(row["Time"])[:-5]))
+
+    return data
+
+
+def load_velocity(path):
+    data = pd.read_csv(path, header=None)
+    data.columns = ["Time", "Velocity"]
+
+    # Replaces epoch time in col 0 with formatted string dateTime
+    for index, row in data.iterrows():
+        data.loc[index, "Time"] = convertTime(int(str(row["Time"])[:-5]))
+
     return data
