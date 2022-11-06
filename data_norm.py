13fe1f6ef47ab2e60ede93508bda2ecbf506ad99
sarthak giri <girisarthak02@gmail.com>
2022-11-06 12:37:47 -0500

    Added time forcasting

--- data_norm.py
+++ data_norm.py
@@ -5,20 +5,17 @@ import pandas as pd
 
 def norm_oneweek(data):
     data.Time = pd.to_datetime(data.Time)
-    data.Temp = pd.to_numeric(data.Temp)
-    result = data.resample('W-FRI', on="Time").Temp.describe()
+    result = data.resample('W-FRI', on="Time").Value.mean().to_frame()
     return result
 
 
 def norm_oneday(data):
     data.Time = pd.to_datetime(data.Time)
-    data.Temp = pd.to_numeric(data.Temp)
-    result = data.resample('D', on="Time").Temp.mean()
+    result = data.resample('D', on="Time").Value.mean().to_frame()
     return result
 
 
 def norm_onehour(data):
     data.Time = pd.to_datetime(data.Time)
-    data.Temp = pd.to_numeric(data.Temp)
-    result = data.resample('H', on="Time").Temp.mean()
+    result = data.resample('H', on="Time").Value.mean().to_frame()
     return result
---
+++ timeSeriesForecasting.py
@@ -0,0 +1,34 @@
+import DataLoading
+from statsmodels.tsa.arima.model import ARIMA
+from statsmodels.tsa.stattools import adfuller
+import pandas as pd
+from pmdarima import auto_arima
+import warnings
+
+warnings.filterwarnings("ignore")
+
+
+def load_temps():
+    data = pd.read_csv(r"/Users/sarthakgiri/Documents/GitHub/KCFHack/Data_set/HackPSU/Fan 1/Temperature.csv",
+                       header=None)
+    data.columns = ["Time", "Temp"]
+    return data
+
+
+Data = load_temps()
+
+
+def ad_test(data):
+    result = adfuller(data['Temp'], autolag='AIC')
+    print("1. ADF : ", result[0])
+    print("2. P-Value : ", result[1])
+    print("3. Num Of Lags : ", result[2])
+    print("4. Nm Of Observations Used For ADF Regression and Critical Values Calculation:", result[3])
+    print("5. Critical Values :")
+    for key, val in result[4].items():
+        print("\t", key, ": ", val)
+
+
+# stepwise_fit = auto_arima(Data['avgTemp'], trace=True, suppress_warnings=True)
+# stepwise_fit.summary()
+ad_test(Data)
