c69ace80d95b88efe560c2bcfaf45d41b6e1ebd7
MrSinister8632 <yash.saxena2001@gmail.com>
2022-11-06 01:23:24 -0400

    Update data_norm.py

--- data_norm.py
+++ data_norm.py
@@ -3,6 +3,10 @@ import DataLoading
 import pandas as pd
 
 data = DataLoading.load_temps()
+endtime = data.tail()
+print(endtime)
+def norm_oneweek():
+    temp = data.filter(like='2022 Oct ')
 
 def norm_oneday():
     data['mean'] = data.groupby(pd.cut(data['Temp'], range(data.loc[0, "Time"], data.loc[0, "Time"] + constants.ONEDAY)))['Temp'].transform('mean')
@@ -12,7 +16,4 @@ def norm_oneweek():
     data['mean'] = data.groupby(pd.cut(data['temp'], range(data.loc[0, "Time"], data.loc[0, "Time"] + constants.ONEWEEK)))['temp'].transform('mean')
     return("one week")
 
-print(norm_oneday())
-#def norm_onemonth():
- #   print("one month")
 
